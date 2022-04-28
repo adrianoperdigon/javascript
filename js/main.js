6bfc3a3b0e77bb0cade01bc0291dd741cabe8b8e
adrianoperdigon <99081346+adrianoperdigon@users.noreply.github.com>
2022-04-28 19:37:49 -0300

    Add files via upload
    primer desafio js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+
+
+for (let dia = 1; dia <=7; dia++) {
+ 
+    switch (dia) {
+        case 1: console.log("El 1 es lunes")
+        break;
+        case 2: console.log("El 2 es martes")
+        break;
+        case 3: console.log("El 3 es miercoles")
+        break;
+        case 4: console.log("El 4 es jueves")
+        break;
+        case 5: console.log("El 5 es viernes")
+        break;
+        case 6: console.log("El 6 es sabado")
+        break;
+        case 7: console.log("El 7 es domingo")
+        break;
+    
+        
+    }
+    
+    
+}
