8218d5a556dc3dcadc83f67177385516d17c685d
adrianoperdigon <99081346+adrianoperdigon@users.noreply.github.com>
2022-05-28 14:34:13 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,121 @@
 console.log("Bienvenido")
 
+const inventario =[
+{
+  nombre: "producto1",
+  id:1,
+  precio:500,
+  img:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/377px-A_small_cup_of_coffee.JPG"
 
+},
+{
+  nombre: "producto2",
+  id:2,
+  precio:1500,
+  img:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/377px-A_small_cup_of_coffee.JPG"
+
+},
+{
+  nombre: "producto3",
+  id:3,
+  precio:2500,
+  img:"https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/A_small_cup_of_coffee.JPG/377px-A_small_cup_of_coffee.JPG"
+}
+]
+
+const divCaja = document.getElementById("caja")
+const divCarrito = document.getElementById("carrito")
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+//funcion que recorre el array y crea un html por cada objeto
+
+function crearCardProducto(){
+  inventario.forEach(element => {
+    divCaja.innerHTML += `
+    
+    <div id="caja">
+    <div class="col-sm-4">
+    <div class="card" style="width: 33vw;">
+    <img src="${element.img}" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">${element.nombre}</h5>
+      <p class="card-text">$${element.precio}</p>
+    
+      <button id="btn${element.id}"text">Agregar</button>
+
+
+    </div>
+    </div>
+    </div>
+  </div>`;
+
+  });
+  //funcion que recorre el array y ejecuta una accion por cada evento click
+    inventario.forEach((producto => {
+      document.querySelector(`#btn${producto.id}`).addEventListener("click", ()=>{
+        enviarAlCarrito(producto)
+      })
+      
+     
+  
+    }) );
+}
+
+//funcion que envia el producto al carrito
+function enviarAlCarrito(producto){
+  let existe = carrito.some((element => element.id === producto.id))
+  if(!existe){
+    carrito.push(producto);
+    producto.cantidad = 1;
+
+    }else{
+      producto.cantidad++
+    }
+    pintarCarrito();
+  }
+
+function pintarCarrito(){
+  divCarrito.innerHTML = "";
+  carrito.forEach(element=>{
+  divCarrito.innerHTML +=`
+  <div id="caja">
+    <div class="col-sm-4">
+    <div class="card" style="width: 33vw;">
+    <img src="${element.img}" class="card-img-top" alt="">
+    <div class="card-body">
+      <h5 class="card-title">${element.nombre}</h5>
+      <p class="card-text">$${element.precio}</p>
+      <p class="card-text">Cantidad: ${element.cantidad}</p>
+
+      <button id="borrar${element.id}">Borrar</button>  
+
+
+    </div>
+    </div>
+    </div>
+  </div>`;
+});
+localStorage.setItem("carrito", JSON.stringify(carrito));
+borrarProducto()
+}
+
+function borrarProducto() {
+  carrito.forEach((producto) => {
+    document
+      .querySelector(`#borrar${producto.id}`)
+      .addEventListener("click", () => {
+        carrito = carrito.filter((element) => element.id !== producto.id);
+        pintarCarrito();
+      });
+  });
+}
+
+
+  crearCardProducto();
+
+
+
+//cards creacion producto
 const inputNombre = document.querySelector("#nombre"),
   inputPrecio = document.querySelector("#precio"),
   inputImg = document.querySelector("#img"),
@@ -8,6 +123,8 @@ const inputNombre = document.querySelector("#nombre"),
   contenedor = document.querySelector("#contenedor");
 
 const productos = [];
+
+
 
 class Producto {
   constructor(nombre,precio, img) {
@@ -31,7 +148,7 @@ function crearHTML() {
   let html;
   for (const producto of productos) {
     html = `
-    <div class="row">
+    <div>
   <div class="col-sm-3">
   <div class="card" style="width: 18rem;">
   <img src="${producto.img}" class="card-img-top" alt="">
@@ -45,9 +162,6 @@ function crearHTML() {
 </div>`;
 
 
-
-
-  
     //console.log(productos);
   }
   contenedor.innerHTML += html;
@@ -57,3 +171,5 @@ btnGuardar.addEventListener("click", () => {
   guardarProducto(product);
   crearHTML(productos);
 });
+
+
